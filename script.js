e790b5db8e0fdbd7b55f31914d3b51bbc6f888de
SihleAddam07 <sihleaddam07@gmail.com>
2025-03-09 19:09:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const poem = document.querySelector(".poem");
-    poem.addEventListener("mouseover", function() {
-        poem.style.color = "#d63384";
-        poem.style.transform = "scale(1.05)";
-    });
-    poem.addEventListener("mouseout", function() {
-        poem.style.color = "#333";
-        poem.style.transform = "scale(1)";
-    });
-});
+function respond(answer) {
+    document.getElementById("responseMessage").innerText = 
+        "You chose: " + answer + " ❤️\nScreenshot and send to Sihle!";
+}
